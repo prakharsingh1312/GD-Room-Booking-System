253caf1a3d19065249a223b1d12a132d2258bc8c
Prakhar Singh <prakharsingh13@gmail.com>
2019-08-06 18:15:43 +0530

    fix

--- phpmyreservation/login.php
+++ phpmyreservation/login.php
@@ -11,8 +11,8 @@ if(isset($_GET['login']))
 }
 elseif(isset($_GET['verify'])&&isset($_GET['token'])&&isset($_GET['hash']))
 {
-	$token=mysqli_real_escape_string($_GET['token']);
-	$hash=mysqli_real_escape_string($_GET['hash']);
+	$token=mysqli_real_escape_string($dbconfig,$_GET['token']);
+	$hash=mysqli_real_escape_string($dbconfig,$_GET['hash']);
 	if(verify_account($token,$hash)==1)
 		echo'<script type="text/javascript">alert("Your email address has been verified you can now login.");</script>';
 	else
