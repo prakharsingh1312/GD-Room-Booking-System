3de7a279f30e46fe1b51ce0ed2c36d8d1b1828b2
ccstiet <ccs@thapar.edu>
2019-10-15 16:44:05 +0530

    bugs fixed

--- phpmyreservation/about.php
+++ phpmyreservation/about.php
@@ -8,8 +8,16 @@ include_once('main.php');
 <div class="box_top_div"><a href="#">Start</a> &gt; About</div>
 <div class="box_body_div">
 
-<p class="center_p"><?php echo global_project_name . ' ' . global_project_version ?></p>
-	<p class="center_p"><b>Created By:</b><br>Prakhar Singh<br>Raghav Virmani<br>Mankaran Singh<br>Kush Daga</p>
+	<p class="center_p"><b>Application Developed By:</b><br><br>
+
+		Prakhar Singh (<a href="mailto:psingh4_be18@thapar.edu">psingh4_be18@thapar.edu</a>)
+
+
+		<br>
+
+		Raghav Virmani (<a href="mailto:rvirmani_be18@thapar.edu">rvirmani_be18@thapar.edu</a>)
+	</p>
+<p class="center_p"><p class="center_p"><b>For:</b></p>Nava Navalanda Central Library, Thapar Institute of Engineering and Technology</p></p>
 <!--<p class="center_p"><a href="<?php echo global_project_website; ?>" target="_blank">Visit website</a></p>-->
 
 </div></div>
