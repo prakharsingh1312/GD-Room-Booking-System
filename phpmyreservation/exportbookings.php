f9d3c4e696133014b8e5d246e244f829c594d6ea
Prakhar Singh <prakharsingh13@gmail.com>
2019-10-19 14:29:21 +0530

    fix

--- phpmyreservation/exportbookings.php
+++ phpmyreservation/exportbookings.php
@@ -12,31 +12,41 @@ include_once('main.php');
 include_once('config.php');
 $dbconfig=mysqli_connect(global_mysqli_server, global_mysqli_user, global_mysqli_password,global_mysqli_database)or die('<span class="error_span"><u>mysqli error:</u> ' . htmlspecialchars(mysqli_error($dbconfig)) . '</span>');
 
-	$query = mysqli_query($dbconfig,"SELECT * FROM `phpmyreservation_groups`,`phpmyreservation_reservations`,`phpmyreservation_roomdetails` WHERE group_id=reservation_group_id AND reservation_room_id=room_id ORDER BY reservation_year DESC,reservation_week DESC,reservation_time DESC,reservation_room_id,reservation_check_in DESC")or die('<span class="error_span"><u>mysqli error:</u> ' . htmlspecialchars(mysqli_error($dbconfig)) . '</span>');
+	global $dbconfig;
+	$query1=mysqli_query($dbconfig,"SELECT member_group_id,count(*) from ".global_mysqli_group_members_table." GROUP by member_group_id");
+	$group_size=array();
+	while($result=mysqli_fetch_array($query1)){
+		$group_size[$result['member_group_id']]=$result['count(*)'];
+	}
+	$query = mysqli_query($dbconfig,"SELECT * FROM " . global_mysqli_groups_table . ",".global_mysqli_reservations_table.",".global_mysqli_room_details_table." WHERE group_id=reservation_group_id AND reservation_room_id=room_id ORDER BY reservation_year DESC,reservation_week DESC,reservation_time DESC,reservation_room_id,reservation_check_in DESC")or die('<span class="error_span"><u>mysqli error:</u> ' . htmlspecialchars(mysqli_error($dbconfig)) . '</span>');
 
-	$users = '<table id="users_table" ><tr><th>ID</th><th>Name</th><th>Members</th><th>Reservation</th><th>Room</th><th>Check In Time</th><th>Check Out Time</th><th></th></tr>';
+	$users = '<table id="users_table" border="1"><tr><th>Group ID</th><th>Group Name</th><th>Reservation Date</th><th>Room</th><th>Floor</th><th>Check In Time</th><th>Check Out Time</th><th>Student Name</th><th>Student Roll No.</th><th>Student Branch</th><th>Student Email</th><th>Student Mobile Number</th></tr>';
 
 	while($user = mysqli_fetch_array($query))
 	{
 		
 		$time="Time:".$user['reservation_time']."<br>".date("d-M-Y", strtotime($user['reservation_year']."W".$user['reservation_week']."-".$user['reservation_day']));
-		$users .= '<tr id="user_tr_' . $user['group_id'] . '"><td><label for="user_radio_' . $user['group_id'] . '">' . $user['group_id'] . '</label></td><td><label for="user_radio_' . $user['group_id'] . '">' . $user['group_name'] . '</label></td><td>' . $time . '</td>	<td>'.$user['room_name'].'</td><td>'.$user['reservation_check_in'].'</td><td>'.$user['reservation_check_out'].'</td><td><input type="radio" class=" blue_button reservation_details_radio" id="'.$user['reservation_id'].'" value="'.$user['reservation_id'].'" name="reservation_details_radio"></td></tr>';
-		}
-	
-
-	$users .= '</table><br><br>
-	<div style="display: none">
-	<input type="button" class=" blue_button " id="reservation_details_button" value="Details">
-	&nbsp;&nbsp;&nbsp;<input type="button" class=" blue_button " id="reservation_check_in_button" value="Mark Check In">
-	&nbsp;&nbsp;&nbsp;<input type="button" class=" button " id="reservation_check_out_button" value="Mark Check Out"> </div>';
-
-	echo $users;
-	echo "<button id='button-a'>Create Excel</button>";
-	
-	echo'
-	
-	</div></div>
-	';
+		$users .= '<tr id="user_tr_' . $user['group_id'] . '">
+		<td rowspan="'.$group_size[$user['group_id']].'"><label for="user_radio_' . $user['group_id'] . '">' . $user['group_id'] . '</label></td><td rowspan="'.$group_size[$user['group_id']].'"><label for="user_radio_' . $user['group_id'] . '">' . $user['group_name'] . '</label></td>
+		<td rowspan="'.$group_size[$user['group_id']].'">' . $time . '</td>
+		<td rowspan="'.$group_size[$user['group_id']].'">'.$user['room_name'].'</td>
+		<td rowspan="'.$group_size[$user['group_id']].'">'.$user['Floor'].'</td>
+		<td rowspan="'.$group_size[$user['group_id']].'">'.$user['reservation_check_in'].'</td>
+		<td rowspan="'.$group_size[$user['group_id']].'">'.$user['reservation_check_out'].'</td>
+		';
+	$query3=mysqli_query($dbconfig,"SELECT * FROM ".global_mysqli_group_members_table.",".global_mysqli_users_table.",".global_mysqli_branches_table." WHERE user_id=member_user_id and member_group_id={$user['group_id']} and branch_id=user_branch_id");
+		$i=0;
+	while($result=mysqli_fetch_array($query3)){
+		if($i!=0)
+		$users.='<tr>';
+		$i++;
+		$users=$users.'<td>'.$result['user_name'].'</td>
+		<td>'.$result['user_roll_no'].'</td>
+		<td>'.$result['branch_code'].'</td>
+		<td>'.$result['user_email'].'</td>
+		<td>'.$result['user_mobile_no'].'</td>
+		</tr>
+		';
 
 
  ?>
