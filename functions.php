ee45d63b0d48c724c2bc8c2fbf631325492eb4c7
Prakhar Singh <prakharsingh13@gmail.com>
2019-11-18 17:37:40 +0530

    fix

--- functions.php
+++ functions.php
@@ -1132,7 +1132,7 @@ function show_room_availability($week,$day,$global_times){
 		$details.='<tr><th class="reservation_time_th">' . $room['room_name'] . '</th>';
 		foreach($global_times as $time)
 		{
-			$query=mysqli_query($dbconfig,"SELECT * FROM ".global_mysqli_reservations_table." WHERE reservation_room_id={$room['room_id']} and reservation_week=$week and reservation_day=$day and reservation_time=$time");
+			$query=mysqli_query($dbconfig,"SELECT * FROM ".global_mysqli_reservations_table." WHERE reservation_room_id={$room['room_id']} and reservation_week=$week and reservation_day=$day and reservation_time='$time'");
 			//$details.="SELECT * FROM ".global_mysqli_reservations_table." WHERE reservation_room_id={$room['room_id']} and reservation_week=$week and reservation_day=$day and reservation_time='$time'";
 			if(mysqli_num_rows($query)>0)
 			{
